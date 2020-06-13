207a17dd23cbbb1cc4a353d849dc0f8f0390ca53
Taben Malik <tabenjmalik@gmail.com>
2020-06-13 14:36:17 +0000

    [001] Created three solutions to Euler 001. Added timing print statements to results.

--- project_euler/euler_001/euler_001.py
+++ project_euler/euler_001/euler_001.py
@@ -1,8 +1,32 @@
+from functools import reduce
+import operator
+
+import numpy as np
+
+MAX_NUM=1000
 
 def solution_01():
     total = 0
-    for i in range(0, 10):
+    for i in range(0, MAX_NUM):
         if i % 5 == 0 or i % 3 == 0:
             total += i
+
+    return total
+
+
+def solution_02():
+    nums = range(0, MAX_NUM)
+    multiples_func = lambda x: x % 5 == 0 or x % 3 == 0
+    total = reduce(operator.add, filter(multiples_func, nums))
+
+    return total
+
+
+def solution_03():
+    num_3s = np.arange(0, MAX_NUM, 3)
+    num_5s = np.arange(0, MAX_NUM, 5)
+    num_15s = np.arange(0, MAX_NUM, 15)
+
+    total = np.sum(num_3s) + np.sum(num_5s) - np.sum(num_15s)
 
     return total

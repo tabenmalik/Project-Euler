2195a49e8a4d41b01f40f26cc7ccf21d82609e76
Taben Malik <tabenjmalik@gmail.com>
2020-06-14 22:23:59 +0000

    [010] Added faster sieve of eratosthenes solution

--- project_euler/euler_010/euler_010.py
+++ project_euler/euler_010/euler_010.py
@@ -1,4 +1,6 @@
-from project_euler import misc
+import math
+
+from project_euler.misc import prime_seq
 
 ANSWER = 142913828922
 
@@ -6,6 +8,56 @@ MAX_NUM = 2_000_000
 
 
 def solution_01():
-    ps = misc.prime_seq(under=MAX_NUM)
+    ps = prime_seq(under=MAX_NUM)
 
     return sum(ps)
+
+
+def sieve_of_eratosthenes_naive(under):
+    bools = [False for i in range(0, under)]
+    bools[0] = True
+    bools[1] = True
+
+    for i in range(4, under, 2):
+        bools[i] = True
+
+    for i in range(3, math.floor(math.sqrt(under))):
+        if not bools[i]:
+            for j in range(i*i, under, 2*i):
+                bools[j] = True
+
+    primes = [i for i, status in enumerate(bools) if not status]
+
+    return primes
+
+def solution_02():
+    """
+    Solution provided by Project Euler
+    """
+    primes = sieve_of_eratosthenes_naive(MAX_NUM)
+    
+    return sum(primes)
+
+
+def sieve_of_eratosthenes_fast(under):
+    sieve_len = int((under - 1) // 2) + 1 
+    sieve = [False for i in range(0, sieve_len)]
+
+    check_limit = (math.floor(math.sqrt(under)) - 1) // 2
+    check_limit = int(check_limit) + 1
+
+    for i in range(1, check_limit):
+        if not sieve[i]:
+            for j in range(2*i*(i+1), sieve_len, (2*i)+1):
+                sieve[j] = True
+
+    primes = [2]
+    primes.extend([(2*i) + 1 for i in range(1, sieve_len) if not sieve[i]])
+
+    return primes 
+
+
+def solution_03():
+    primes = sieve_of_eratosthenes_fast(MAX_NUM)
+
+    return sum(primes)

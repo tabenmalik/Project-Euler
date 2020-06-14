f94b6bb2944b8dc5c4a8aa468a6701b5883303c8
Taben Malik <tabenjmalik@gmail.com>
2020-06-14 03:10:45 +0000

    [009] Solved Euler 009

---
+++ project_euler/euler_009/euler_009.py
@@ -0,0 +1,17 @@
+import itertools
+from functools import reduce
+import operator
+
+def solution_01():
+    SUM_NUM = 1000
+
+    nums = range(SUM_NUM)
+    triplets = ((i, j, SUM_NUM-i-j) for i in range(1, SUM_NUM) 
+                for j in range(i+1, SUM_NUM-i))
+
+    triplets = filter(lambda t: t[0]**2 + t[1]**2 == t[2]**2, triplets)
+    triplets = list(triplets)
+    
+    assert(len(triplets) == 1)
+    prod = reduce(operator.mul, triplets[0])
+    return prod

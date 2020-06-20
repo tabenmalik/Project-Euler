e55871b4e032debfe2e9002625c6a2b39b2fbf42
Taben Malik <tabenjmalik@gmail.com>
2020-06-20 13:40:20 -0500

    Solved Euler 019

---
+++ project_euler/euler_problems/euler_019.py
@@ -0,0 +1,11 @@
+import datetime as dt
+
+def solution_01():
+    count = 0
+    for year in range(1901, 2001):
+        for month in range(1, 13):
+            date = dt.datetime(year, month, 1)
+            if date.weekday() == 6:
+                count += 1
+
+    return count

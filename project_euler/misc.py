987e3e87b697432db0362a5cb3e836a5e95e2107
Taben Malik <tabenjmalik@gmail.com>
2020-06-21 21:00:33 -0500

    Added previous solution for Euler 012

--- project_euler/misc.py
+++ project_euler/misc.py
@@ -105,3 +105,38 @@ def sum_of_sqrs(n):
 
 def sum_of_cubes(n):
     return int(((n * n) * (n + 1) * (n + 1)) / 4)
+
+
+def sieve_of_eratosthenes_fast(under):
+    sieve_len = int((under - 1) // 2) + 1 
+    sieve = [False for i in range(0, sieve_len)]
+
+    check_limit = (math.floor(math.sqrt(under)) - 1) // 2
+    check_limit = int(check_limit) + 1
+
+    for i in range(1, check_limit):
+        if not sieve[i]:
+            for j in range(2*i*(i+1), sieve_len, (2*i)+1):
+                sieve[j] = True
+
+    primes = [2]
+    primes.extend([(2*i) + 1 for i in range(1, sieve_len) if not sieve[i]])
+
+    return primes 
+
+def sieve_of_eratosthenes_naive(under):
+    bools = [False for i in range(0, under)]
+    bools[0] = True
+    bools[1] = True
+
+    for i in range(4, under, 2):
+        bools[i] = True
+
+    for i in range(3, math.floor(math.sqrt(under))):
+        if not bools[i]:
+            for j in range(i*i, under, 2*i):
+                bools[j] = True
+
+    primes = [i for i, status in enumerate(bools) if not status]
+
+

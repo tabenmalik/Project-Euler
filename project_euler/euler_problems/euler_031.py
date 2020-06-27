5f6491259e35e3de944262f38f247f7b4b58981f
Taben Malik <tabenjmalik@gmail.com>
2020-06-27 20:58:14 +0000

    Added Project Euler solutions for 031

--- project_euler/euler_problems/euler_031.py
+++ project_euler/euler_problems/euler_031.py
@@ -4,7 +4,7 @@ MAX_NUM = 200
 
 COINS = [1, 2, 5, 10, 20, 50, 100, 200]
 COIN_SUM_CACHE = {}
-def coin_sums(total):
+def coin_sums(total, coins):
     if total == 1:
         return frozenset([(1,)])
 
@@ -12,13 +12,17 @@ def coin_sums(total):
         return COIN_SUM_CACHE[total]
 
     ways_to_sum = list()
-    for coin in COINS:
+    for coin in coins:
         new_total = total - coin
 
         if new_total == 0:
             ways_to_sum.append(tuple([coin]))
         elif new_total > 0:
-            ways_to_subsum = coin_sums(new_total)
+            new_coins = coins.copy()
+            if new_total < coin:
+                new_coins.remove(coin)
+
+            ways_to_subsum = coin_sums(new_total, new_coins)
             for way in ways_to_subsum:
                 ways_to_sum.append((coin,) + way)
 
@@ -29,4 +33,34 @@ def coin_sums(total):
     return ways_to_sum
 
 def solution_01():
-    return len(coin_sums(MAX_NUM))
+    return len(coin_sums(MAX_NUM, COINS))
+
+
+COIN_WAYS_CAHCE = {}
+def ways(target, avc):
+    if avc <= 1:
+        return 1
+
+    if (target, avc) in COIN_WAYS_CAHCE:
+        return COIN_WAYS_CAHCE[(target, avc)]
+    
+    res = 0
+    while target >= 0:
+        res = res + ways(target, avc - 1)
+        target = target - COINS[avc-1]
+
+    COIN_WAYS_CAHCE[(target, avc)] = res
+    return res
+
+
+def solution_02():
+    print(ways(MAX_NUM, len(COINS)))
+
+
+def solution_03():
+    ways = [0 for _ in range(MAX_NUM+1)]
+    ways[0] = 1
+    for coin in COINS:
+        for j in range(coin, MAX_NUM+1):
+            ways[j] = ways[j] + ways[j-coin]
+    return ways[MAX_NUM]

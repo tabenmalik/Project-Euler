ea79b226a3e5bfe392c37fbeee34aeabd4bf51df
Taben Malik <tabenjmalik@gmail.com>
2020-07-04 19:52:11 +0000

    Solved Euler 035 and 036

---
+++ project_euler/euler_problems/euler_035.py
@@ -0,0 +1,23 @@
+from project_euler.misc import sieve_of_eratosthenes_fast, num_to_digits, digits_to_num, is_prime
+
+SOLUTION = 55
+
+def cycle_digits(num):
+    yield num
+
+    digits = num_to_digits(num)
+    digits = digits[1:] + digits[:1]
+    new_num = digits_to_num(digits)
+    while new_num != num:
+        yield new_num
+        digits = digits[1:] + digits[:1]
+        new_num = digits_to_num(digits)
+
+
+def solution_01():
+    limit = 1_000_000
+    primes = set(sieve_of_eratosthenes_fast(limit))
+
+    num_cycles = map(lambda x: (x, set(cycle_digits(x))), primes)
+    prime_cycles = filter(lambda x: x[1].issubset(primes), num_cycles)
+    return len(list(prime_cycles))

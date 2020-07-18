2ec6c4d8fb965d809e746a0b5bb812f65607b7d0
Taben Malik <tabenjmalik@gmail.com>
2020-07-18 16:18:02 +0000

    Solved Euler 047

---
+++ project_euler/euler_problems/euler_047.py
@@ -0,0 +1,20 @@
+from project_euler.misc import prime_factors
+
+SOLUTION = '134043'
+
+
+def solve():
+    
+    distinct_prime_factors = 4
+    
+    consecutive_nums = []
+    i = 4
+    while True:
+        factors = set(prime_factors(i))
+        if len(factors) == distinct_prime_factors:
+            consecutive_nums.append(i)
+            if len(consecutive_nums) == distinct_prime_factors:
+                return str(consecutive_nums[0])
+        else:
+            consecutive_nums = []
+        i += 1

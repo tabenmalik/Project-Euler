66066f7fac86899b9fdd8bf62e45b85658a46fe0
Taben Malik <tabenjmalik@gmail.com>
2020-08-08 01:48:00 +0000

    Solved Euler 059

---
+++ project_euler/euler_problems/euler_059.py
@@ -0,0 +1,37 @@
+import itertools
+import os
+
+SOLUTION = '129448'
+
+
+def read_number_file():
+    this_dir, _ = os.path.split(__file__)
+    
+    nums = []
+    with open(os.path.join(this_dir, 'euler_059.txt')) as fhdl:
+        nums = list(map(int, fhdl.read().split(',')))
+
+    return nums
+
+
+def decrypt(text, password):
+    chunks = [text[i:i+len(password)] for i in range(0, len(text), len(password))]
+    decrypted_chunks = []
+    for chunk in chunks:
+        decrypted_chunks.append(list(map(lambda x, y: x ^ y, chunk, password)))
+    
+    flattened_decrypt = [num for chunk in decrypted_chunks for num in chunk]
+    decrypted_ascii = list(map(chr, flattened_decrypt))
+    return ''.join(decrypted_ascii)
+
+
+def solve():
+    nums = read_number_file()
+
+    lower_case_ascii = [ord('a') + i for i in range(26)]
+    passwords = list(itertools.permutations(lower_case_ascii, r=3))
+    
+    for password in passwords:
+        message = decrypt(nums, password)
+        if ' the ' in message:
+            return sum(map(ord, message))

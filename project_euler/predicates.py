8c29c9932b42b0e11d7d58cd82bf59fd2cac3dda
Taben Malik <tabenjmalik@gmail.com>
2021-06-27 16:47:47 -0500

    New predicates module for special unary/binary predicate creation

---
+++ project_euler/predicates.py
@@ -0,0 +1,9 @@
+"""A module of functions to create predicates."""
+import operator
+
+
+def lt(n):
+    """Creates a less than unary predicate"""
+    def _unary_lt(x):
+        return operator.lt(x, n)
+    return _unary_lt

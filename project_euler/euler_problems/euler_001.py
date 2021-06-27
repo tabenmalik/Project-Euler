4c4d06a0e8d7ff228d36b6e0d2276353e56521f5
Taben Malik <tabenjmalik@gmail.com>
2021-06-27 13:37:39 -0500

    Lint Euler 001

--- project_euler/euler_problems/euler_001.py
+++ project_euler/euler_problems/euler_001.py
@@ -1,9 +1,20 @@
+"""
+Project Euler problem 001: https://projecteuler.net/problem=1
+
+Description:
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
 SOLUTION = '233168'
 
 MAX_NUM = 1000
 
 
 def solve():
+    """Solves Project Euler problem 001"""
     num_3s = range(0, MAX_NUM, 3)
     num_5s = range(0, MAX_NUM, 5)
     num_15s = range(0, MAX_NUM, 15)

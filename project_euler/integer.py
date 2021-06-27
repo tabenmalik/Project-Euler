753d64e624ee82ba64122aea2f810037bb30f94b
Taben Malik <tabenjmalik@gmail.com>
2021-06-27 00:37:27 -0500

    Adding commets to integer.py. Trying to make a dent in the linting debt

--- project_euler/integer.py
+++ project_euler/integer.py
@@ -1,71 +1,92 @@
-"""
-"""
-
+"""Functions for testing or manipulating properties of integers"""
 import math
 
+
 def is_palindrome(num):
-    """
+    """Returns True if the number is a palindrome.
+    A palidrome is a sequence that is reversible.
+    e.g. 11211
+    e.g. 329923
+    TODO: reference
     """
     num_str = str(num)
     reverse_str = num_str[::-1]
     return num_str == reverse_str
 
 
-def trigonal(n):
-    return sum_of_n(n)
+def trigonal(num):
+    """The trigonal function
+    TODO: reference"""
+    return sum_of_n(num)
 
 
-def is_trigonal(t):
-    n1 = -0.5 + math.sqrt(0.25 + (2 * t))
-    n2 = -0.5 - math.sqrt(0.25 + (2 * t))
+def is_trigonal(num):
+    """Returns True if the number is trigonal
+    TODO: reference"""
+    n_1 = -0.5 + math.sqrt(0.25 + (2 * num))
+    n_2 = -0.5 - math.sqrt(0.25 + (2 * num))
 
-    result = n1 > 0 and trigonal(int(n1)) == t
-    result |= n2 > 0 and trigonal(int(n2)) == t
-
-    return result
-
-
-def pentagonal(n):
-    return (n * ((3 * n) - 1)) // 2
-
-
-def is_pentagonal(p):
-    if p <= 0:
-        return False
-
-    n1 = (0.5 + math.sqrt(0.25 + (6*p))) / 3
-    n2 = (0.5 - math.sqrt(0.25 + (6*p))) / 3
-    
-    result = n1 > 0 and pentagonal(int(n1)) == p
-    result |= n2 > 0 and pentagonal(int(n2)) == p
+    result = n_1 > 0 and trigonal(int(n_1)) == num
+    result |= n_2 > 0 and trigonal(int(n_2)) == num
 
     return result
 
 
-def hexagonal(n):
-    return n * ((2*n) - 1)
+def pentagonal(num):
+    """The pentagonal function.
+    TODO: reference"""
+    return (num * ((3 * num) - 1)) // 2
 
 
-def is_hexagonal(h):
-    if h <= 0:
+def is_pentagonal(num):
+    """Returns True if the number is pentagonal.
+    TODO: reference"""
+    if num <= 0:
         return False
 
-    n1 = 0.25 * (1 + math.sqrt(1 + (8*h)))
-    n2 = 0.25 * (1 - math.sqrt(1 + (8*h)))
+    n_1 = (0.5 + math.sqrt(0.25 + (6 * num))) / 3
+    n_2 = (0.5 - math.sqrt(0.25 + (6 * num))) / 3
 
-    result = n1 > 0 and hexagonal(int(n1)) == h
-    result |= n2 > 0 and hexagonal(int(n2)) == h
+    result = n_1 > 0 and pentagonal(int(n_1)) == num
+    result |= n_2 > 0 and pentagonal(int(n_2)) == num
 
-    return result 
-
-
-def sum_of_n(n):
-    return int((n * (n + 1)) / 2)
+    return result
 
 
-def sum_of_sqrs(n):
-    return int((((2 * n) + 1) * (n + 1) * n) / 6)
+def hexagonal(num):
+    """The hexagonal function
+    TODO: reference"""
+    return num * ((2 * num) - 1)
 
 
-def sum_of_cubes(n):
-    return int(((n * n) * (n + 1) * (n + 1)) / 4)
+def is_hexagonal(num):
+    """Returns True if the number is hexagonal
+    TODO: reference"""
+    if num <= 0:
+        return False
+
+    n_2 = 0.25 * (1 + math.sqrt(1 + (8 * num)))
+    n_2 = 0.25 * (1 - math.sqrt(1 + (8 * num)))
+
+    result = n_2 > 0 and hexagonal(int(n_2)) == num
+    result |= n_2 > 0 and hexagonal(int(n_2)) == num
+
+    return result
+
+
+def sum_of_n(num):
+    """Returns the sum of all positive integers up to num
+    TODO: reference"""
+    return int((num * (num + 1)) / 2)
+
+
+def sum_of_sqrs(num):
+    """TODO: what does this do?
+    TODO: reference"""
+    return int((((2 * num) + 1) * (num + 1) * num) / 6)
+
+
+def sum_of_cubes(num):
+    """TODO: what does this do?
+    TODO: reference"""
+    return int(((num * num) * (num + 1) * (num + 1)) / 4)

c694704dbc947a45f16ef9af821987fc1aef452f
Taben Malik <tabenjmalik@gmail.com>
2021-06-27 17:36:24 -0500

    Linting and reworking euler 007

--- project_euler/euler_problems/euler_007.py
+++ project_euler/euler_problems/euler_007.py
@@ -1,16 +1,21 @@
-import itertools
+"""
+Project Euler problem 006: https://projecteuler.net/problem=7
 
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+"""
+from project_euler.more_itertools import nth
 from project_euler.sequences import prime_seq
 
 SOLUTION = '104743'
 
 MAX_NUM = 10_001
 
-def solution_01():
-    primes_under_num = itertools.takewhile(lambda x: x[0] < MAX_NUM, enumerate(prime_seq()))
-    _, primes_under_num = zip(*primes_under_num)
-    return list(primes_under_num)[-1]
-
 
 def solve():
-    return str(solution_01())
+    """Solves Project Euler problem 007"""
+    primes = prime_seq()
+    last_prime = nth(primes, MAX_NUM)
+    return str(last_prime)

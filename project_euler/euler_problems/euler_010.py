849676fbcf28880640f557f4af4fb397bc4dbc2f
Taben Malik <tabenjmalik@gmail.com>
2021-07-10 09:19:05 -0500

    Fixing up euler 010

--- project_euler/euler_problems/euler_010.py
+++ project_euler/euler_problems/euler_010.py
@@ -1,6 +1,10 @@
-import math
+"""Project Euler problem 010: https://projecteuler.net/problem=10
 
-from project_euler.sequences import prime_seq
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+"""
+import math
 
 SOLUTION = '142913828922'
 
@@ -8,7 +12,18 @@ MAX_NUM = 2_000_000
 
 
 def sieve_of_eratosthenes_fast(under):
-    sieve_len = int((under - 1) // 2) + 1 
+    """Returns a list primes.
+
+    Uses the Sieve of Eratosthenes methodology for determining all
+    prime numbers under a limit.
+
+    Args:
+        under (int): find the primes below this number
+
+    Returns:
+        list(int): prime numbers
+    """
+    sieve_len = int((under - 1) // 2) + 1
     sieve = [False for i in range(0, sieve_len)]
 
     check_limit = (math.floor(math.sqrt(under)) - 1) // 2
@@ -22,10 +37,10 @@ def sieve_of_eratosthenes_fast(under):
     primes = [2]
     primes.extend([(2*i) + 1 for i in range(1, sieve_len) if not sieve[i]])
 
-    return primes 
+    return primes
 
 
 def solve():
+    """Solves Project Euler problem 010"""
     primes = sieve_of_eratosthenes_fast(MAX_NUM)
-
     return str(sum(primes))

3454e481171fb9c5d707799bcbb8a845ee0d99b4
Taben Malik <tabenjmalik@gmail.com>
2022-05-14 15:18:25 -0500

    Notes

--- project_euler/predicates.py
+++ project_euler/predicates.py
@@ -1,7 +1,7 @@
 """A module of functions to create predicates."""
 import operator
 
-
+# this can be replace with a "partial"
 def lt(n):
     """Creates a less than unary predicate"""
     def _unary_lt(x):

b070f449d2acac8ab051defacc44c14b29855a64
Taben Malik <tabenjmalik@gmail.com>
2025-11-01 20:20:23 -0500

    Solved p066

--- src/pe_problems/p066.py
+++ src/pe_problems/p066.py
@@ -1,40 +1,37 @@
-from math import isqrt
-from itertools import count, takewhile, islice
-from typing import Generator
+import math
 
-from pe.predicates import le
-
-SOLUTION = ""
-
-def squares(start: int = 1) -> Generator[int, None, None]:
-    for i in count(start):
-        yield i**2
+SOLUTION = "661"
 
 
-def nonsquares() -> Generator[int, None, None]:
-    for i in count(2):
-        if isqrt(i) ** 2 != i:
-            yield i
+def issquare(x):
+    return math.isqrt(x) ** 2 == x
 
 
-def diophantine_min_x(D: int) -> int:
-    for m in squares():
-        print("m:", m)
-        d = m * D + 1
-        if isqrt(d) ** 2 == d:
-            return int(isqrt(d))
-    raise Exception("Not expected")
+def next_triple(t, N):
+    m = 1
+    from fractions import Fraction
+
+    while Fraction((t[0] + t[1] * m), t[2]).denominator != 1:
+        m += 1
+    return ((t[0] * m + N * t[1]) // abs(t[2]), (t[0] + t[1] * m) // abs(t[2]), (m * m - N) // t[2])
+
+
+def min_dio(D):
+    a = int(math.isqrt(D) + 1)
+    t = (a, 1, a * a - D)
+    while t[2] != 1:
+        t = next_triple(t, D)
+    return t
 
 
 def solve() -> str:
-    maxx = 3
-    maxD = 2
-
-    for D in takewhile(le(1000), nonsquares()):
-        x = diophantine_min_x(D)
-        print(D, x)
-        if x > maxx:
-            maxx = x
-            maxD = D
-
-    return str(maxD)
+    max_x = 0
+    max_D = 0
+    for D in range(2, 1001):
+        if issquare(D):
+            continue
+        t = min_dio(D)
+        if t[0] > max_x:
+            max_x = t[0]
+            max_D = D
+    return str(max_D)

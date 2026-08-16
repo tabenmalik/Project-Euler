37e640ebe52708502c683a142520a965d1c290cf
Taben Malik <tabenjmalik@gmail.com>
2026-08-16 13:39:02 -0500

    strict mypy: p032.py
    taking the opportunity to rewrite the solution in a clearer way

--- src/pe_problems/p032.py
+++ src/pe_problems/p032.py
@@ -1,40 +1,37 @@
 from __future__ import annotations
 
-import itertools
+from collections.abc import Iterator
+from itertools import permutations
+
+from pe.integer import concat
 
 SOLUTION = "45228"
 
+Partition = tuple[tuple[int, ...], tuple[int, ...], tuple[int, ...]]
 
-def solve() -> str:
-    digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    # digits = [1, 2, 3, 4]
-    all_perms = list(itertools.permutations(digits))
-    perms_columns = list(zip(*all_perms))
 
-    products: set[int] = set()
+def partitions(digits: tuple[int, ...]) -> Iterator[Partition]:
+    # not a generalized partitioning.
     for i in range(1, 5):
         for j in range(i + 1, 6):
-            a = zip(*perms_columns[0:i])
-            b = zip(*perms_columns[i:j])
-            product = zip(*perms_columns[j:])
-
-            a = map(lambda x: list(map(str, x)), a)
-            b = map(lambda x: list(map(str, x)), b)
-            product = map(lambda x: list(map(str, x)), product)
-
-            a = map(lambda x: "".join(x), a)
-            b = map(lambda x: "".join(x), b)
-            product = map(lambda x: "".join(x), product)
-
-            a = map(int, a)
-            b = map(int, b)
-            product = map(int, product)
-
-            actual_products = filter(
-                lambda trip: trip[0] == trip[1] * trip[2], zip(product, a, b),
+            yield (
+                tuple(digits[0:i]),
+                tuple(digits[i:j]),
+                tuple(digits[j:]),
             )
-            actual_products = list(zip(*actual_products))
-            if len(actual_products) > 0:
-                products.update(actual_products[0])
+
+
+def solve() -> str:
+    digits = tuple(range(1, 10))
+
+    products: set[int] = set()
+    for permutation in permutations(digits):
+        for partition in partitions(permutation):
+            multiplicand = concat(partition[0])
+            multiplier = concat(partition[1])
+            product = concat(partition[2])
+
+            if multiplicand * multiplier == product:
+                products.add(product)
 
     return str(sum(products))
